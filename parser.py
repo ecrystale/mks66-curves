ec1bc7eeec1c48d5e334d55f24bbb72380fce8dc
ecrystale <elee19@stuy,edu>
2019-03-12 06:24:55 -0400

    starting own img

--- parser.py
+++ parser.py
@@ -118,4 +118,4 @@ screen = new_screen()
 color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
-parse_file( 'script', edges, transform, screen, color )
+parse_file( 'script2.txt', edges, transform, screen, color )
